5843a620d880ea84be67aad1ee1e3033c4e11adc
Danila Egorov <c4nsero@mail.ru>
2023-03-17 14:11:17 +0300

    ✨ Initial commit ✨

---
+++ go.mod
@@ -0,0 +1,7 @@
+module tgBot
+
+go 1.13
+
+require ( 
+	github.com/spf13/viper v1.9.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"tgBot/mods"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+
+	// Инициализация конфига (токенов)
+	err := mods.InitConfig()
+	if err != nil {
+		log.Fatalf("initConfig error: %s", err)
+		return
+	}
+
+	// Url бота для отправки и приёма сообщений
+	botUrl := "https://api.telegram.org/bot" + viper.GetString("token")
+	offSet := 0
+
+	// Цикл работы бота
+	for {
+
+		// Получение апдейтов
+		updates, err := getUpdates(botUrl, offSet)
+		if err != nil {
+			log.Fatalf("getUpdates error: %s", err)
+		}
+
+		// Обработка апдейтов
+		for _, update := range updates {
+			respond(botUrl, update)
+			offSet = update.UpdateId + 1
+		}
+
+		// Вывод в консоль для тестов
+		// fmt.Println(updates)
+	}
+}
+
+// Функция получения апдейтов
+func getUpdates(botUrl string, offset int) ([]mods.Update, error) {
+
+	// Rest запрос для получения апдейтов
+	resp, err := http.Get(botUrl + "/getUpdates?offset=" + strconv.Itoa(offset))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Запись и обработка полученных данных
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var restResponse mods.TelegramResponse
+	err = json.Unmarshal(body, &restResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return restResponse.Result, nil
+}
+
+// Функция генерации и отправки ответа
+func respond(botUrl string, update mods.Update) {
+
+	// Обработчик команд
+	if update.Message.Text != "" {
+
+		request := append(strings.Split(update.Message.Text, " "), "", "")
+
+		// Вывод реквеста для тестов
+		// fmt.Println("request: \t", request)
+
+		switch request[0] {
+		case "/info":
+			mods.SendInfo(botUrl, update.Message.Chat.ChatId, request[1])
+		case "/start", "/help":
+			mods.Help(botUrl, update.Message.Chat.ChatId)
+		default:
+			mods.SendMsg(botUrl, update.Message.Chat.ChatId, "OwO")
+		}
+
+	} else {
+
+		// Если пользователь отправил не сообщение и не стикер:
+		mods.SendMsg(botUrl, update.Message.Chat.ChatId, "Пока я воспринимаю только текст и стикеры")
+		mods.SendStck(botUrl, update.Message.Chat.ChatId, "CAACAgIAAxkBAAIaImHkPqF8-PQVOwh_Kv1qQxIFpPyfAAJXAAOtZbwUZ0fPMqXZ_GcjBA")
+
+	}
+}
