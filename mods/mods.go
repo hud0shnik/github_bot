264ae74a80ef82d879c6587e48cac456d3287181
Danila Egorov <c4nsero@mail.ru>
2023-04-08 14:21:05 +0300

    update mods: add SendRepo()

--- mods/mods.go
+++ mods/mods.go
@@ -60,6 +60,19 @@ type CommitsResponse struct {
 	Color    int    `json:"color"`
 }
 
+type RepoResponse struct {
+	Success  bool   `json:"success"`
+	Error    string `json:"error"`
+	Username string `json:"username"`
+	Reponame string `json:"reponame"`
+	Commits  string `json:"commits"`
+	Branches string `json:"branches"`
+	Tags     string `json:"tags"`
+	Stars    string `json:"stars"`
+	Watching string `json:"watching"`
+	Forks    string `json:"forks"`
+}
+
 // Функция вывода информации о пользователе GitHub
 func SendInfo(botUrl string, chatId int, username string) {
 
@@ -176,6 +189,58 @@ func SendCommits(botUrl string, chatId int, username, date string) {
 		SendMsg(botUrl, chatId, "Коммитов нет...")
 		SendStck(botUrl, chatId, "CAACAgIAAxkBAAIYG2GzRVNm_d_mVDIOaiLXkGukArlTAAJDAAOtZbwU_-iXZG7hfLsjBA")
 	}
+}
+
+// Функция вывода информации о репозитории
+func SendRepo(botUrl string, chatId int, username, reponame string) {
+
+	// Проверка параметров
+	if username == "" || reponame == "" {
+		SendMsg(botUrl, chatId, "Синтаксис команды:\n\n/repo <b>[username]</b> <b>[reponame]</b>\n\nПример:\n/repo <b>hud0shnik GithubStatsAPI</b>")
+		return
+	}
+
+	// Отправка запроса GithubStats Api
+	resp, err := http.Get("https://githubstatsapi.vercel.app/api/v2/repo?type=string&username=" + username + "&reponame=" + reponame)
+
+	// Проверка на ошибку
+	if err != nil {
+		SendMsg(botUrl, chatId, "Внутренняя ошибка")
+		log.Printf("http.Get error: %s", err)
+		return
+	}
+
+	// Запись респонса
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	var repo = new(RepoResponse)
+	json.Unmarshal(body, &repo)
+
+	// Проверка респонса
+	switch resp.StatusCode {
+	case 200:
+		// При хорошем статусе респонса, продолжение выполнения кода
+	case 404:
+		SendMsg(botUrl, chatId, "Репозиторий не найден")
+		return
+	case 400:
+		SendMsg(botUrl, chatId, "Плохой реквест")
+		return
+	default:
+		SendMsg(botUrl, chatId, "Внутренняя ошибка")
+		return
+	}
+
+	// Отправка данных пользователю
+	SendMsg(botUrl, chatId, fmt.Sprintf("Информация о <b>%s/%s</b>\n"+
+		"Коммитов <b>%s</b>\n"+
+		"Веток <b>%s</b>\n"+
+		"Тегов <b>%s</b>\n"+
+		"Звёзд <b>%s</b>\n"+
+		"Следят <b>%s</b>\n"+
+		"Форков <b>%s</b>",
+		repo.Username, repo.Reponame, repo.Commits, repo.Branches, repo.Tags, repo.Stars, repo.Watching, repo.Forks))
+
 }
 
 // Функция вывода списка всех команд
